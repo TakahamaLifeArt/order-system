8a449d24d786d8dcd0a201937b9cd5c5abdd0b5b
Ks <ks.desk@gmail.com>
2018-01-12 14:07:15 +0900

    first commit

---
+++ home/system/php_libs/edi_ordering.php
@@ -0,0 +1,38 @@
+<?php
+/*
+*	EDI発注
+*	log		: 2014-01-09 created
+*			: 2014-11-14 トムスとキャブ双方に対応
+*/
+	require_once 'http.php';
+	if(isset($_POST['orders_id'], $_POST['maker'])) {
+		if($_POST['maker']=='toms'){
+			try{
+				$http = new HTTP('http://takahamalifeart.com/toms/toms_order.php');
+				$param = array(
+					'orders_id'=>$_POST['orders_id'],
+					'deliver'=>$_POST['deliver'],
+					'destination'=>$_POST['destination'],
+					'saturday'=>$_POST['saturday'],
+					'holiday'=>$_POST['holiday'],
+				);
+				$reply = $http->request('POST', $param);
+			}catch (Exception $e) {
+				$reply = 'ERROR: '.$e;
+			}
+		}else if($_POST['maker']=='cab'){
+			try{
+				$http = new HTTP('http://takahamalifeart.com/cab/cab_order.php');
+				$param = array(
+					'orders_id'=>$_POST['orders_id'],
+					'destination'=>$_POST['destination'],
+					'cab_note'=>$_POST['cab_note'],
+				);
+				$reply = $http->request('POST', $param);
+			}catch (Exception $e) {
+				$reply = 'ERROR: '.$e;
+			}
+		}
+	}
+	echo $reply;
+?>
