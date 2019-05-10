d9e90a5ac9e14160e19298cc0a34dceeec76a4f3
Ks <ks.desk@gmail.com>
2019-05-10 12:08:59 +0900

    update EDI発注の送信をHTTPSで統一

--- home/system/php_libs/edi_ordering.php
+++ home/system/php_libs/edi_ordering.php
@@ -3,12 +3,13 @@
 *	EDI発注
 *	log		: 2014-01-09 created
 *			: 2014-11-14 トムスとキャブ双方に対応
+*			: 2019-05-10 HTTPSに統一
 */
 	require_once 'http.php';
 	if(isset($_POST['orders_id'], $_POST['maker'])) {
 		if($_POST['maker']=='toms'){
 			try{
-				$http = new HTTP('http://takahamalifeart.com/toms/toms_order.php');
+				$http = new HTTP('https://takahamalifeart.com/toms/toms_order.php');
 				$param = array(
 					'orders_id'=>$_POST['orders_id'],
 					'deliver'=>$_POST['deliver'],
@@ -22,7 +23,7 @@
 			}
 		}else if($_POST['maker']=='cab'){
 			try{
-				$http = new HTTP('http://takahamalifeart.com/cab/cab_order.php');
+				$http = new HTTP('https://takahamalifeart.com/cab/cab_order.php');
 				$param = array(
 					'orders_id'=>$_POST['orders_id'],
 					'destination'=>$_POST['destination'],
