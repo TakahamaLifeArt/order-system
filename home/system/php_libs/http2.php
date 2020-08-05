c75ed4b2468fd4984add4b6a3674b5aa4915ed0b
Ks <ks.desk@gmail.com>
2020-08-06 06:11:50 +0900

    test actions

--- home/system/php_libs/http2.php
+++ home/system/php_libs/http2.php
@@ -1,71 +1,73 @@
 <?php
-/*
-*	HTTP Request
-*	charset utf-8
-*
-*/
+/**
+ * HTTP Request
+ * charset utf-8
+ */
 
+ namespace php_libs;
 
-class HTTP2 {
+class HTTP2
+{
 
-	private $url;
-	
-	public function __construct($args){
-		$this->url = $args;
-	}
+    private $url;
+
+	public function __construct($args)
+	{
+        $this->url = $args;
+    }
 
     public function request($method, $params = array()){
-    	$url = $this->url;
-	    $data = http_build_query($params);
-	    if($method == 'GET') {
-	        $url = ($data != '')?$url.'?'.$data:$url;
-	    }
-	 
-	    $ch = curl_init($url);
-	 
-	    if($method == 'POST'){
-	        curl_setopt($ch,CURLOPT_POST,1);
-	        curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
-	    }
-	 
-	    //curl_setopt($ch, CURLOPT_HEADER,true); //header情報も一緒に欲しい場合
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	    //curl_setopt($ch, CURLOPT_TIMEOUT, 2);
-	    $res = curl_exec($ch);
-	 
-	    //ステータスをチェック
-	    $respons = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-	    if(preg_match("/^(404|403|500)$/",$respons)){
-	        return false;
-	    }
-	 
-	    return $res;
-	}
-	
-	public function request2($method, $params = array()){
-		$url = $this->url;
-	    $data = http_build_query($params);
-	    $header = Array("Content-Type: application/x-www-form-urlencoded");
-	    $options = array('http' => Array(
-	        'method' => $method,
-	        'header'  => implode("\r\n", $header),
-	    ));
-	 
-	    //ステータスをチェック / PHP5専用 get_headers()
-	    $respons = get_headers($url);
-	    if(preg_match("/(404|403|500)/",$respons['0'])){
-	        return false;
-	    }
-	 
-	    if($method == 'GET') {
-	        $url = ($data != '')?$url.'?'.$data:$url;
-	    }elseif($method == 'POST') {
-	        $options['http']['content'] = $data;
-	    }
-	    $content = file_get_contents($url, false, stream_context_create($options));
-	 
-	    return $content;
-	}
+        $url = $this->url;
+        $data = http_build_query($params);
+        if($method == 'GET') {
+            $url = ($data != '')?$url.'?'.$data:$url;
+        }
+
+        $ch = curl_init($url);
+
+        if($method == 'POST'){
+            curl_setopt($ch,CURLOPT_POST,1);
+            curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+        }
+
+        //curl_setopt($ch, CURLOPT_HEADER,true); //header情報も一緒に欲しい場合
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        //curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+        $res = curl_exec($ch);
+
+        //ステータスをチェック
+        $respons = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if(preg_match("/^(404|403|500)$/",$respons)){
+            return false;
+        }
+
+        return $res;
+    }
+
+    public function request2($method, $params = array()){
+        $url = $this->url;
+        $data = http_build_query($params);
+        $header = Array("Content-Type: application/x-www-form-urlencoded");
+        $options = array('http' => Array(
+            'method' => $method,
+            'header'  => implode("\r\n", $header),
+        ));
+
+        //ステータスをチェック / PHP5専用 get_headers()
+        $respons = get_headers($url);
+        if(preg_match("/(404|403|500)/",$respons['0'])){
+            return false;
+        }
+
+        if($method == 'GET') {
+            $url = ($data != '')?$url.'?'.$data:$url;
+        }elseif($method == 'POST') {
+            $options['http']['content'] = $data;
+        }
+        $content = file_get_contents($url, false, stream_context_create($options));
+
+        return $content;
+    }
 }
 ?>
