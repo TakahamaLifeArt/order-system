bd09c91aba7300b166c71d88a8e3babd06cf9a36
doozor <ks.desk@gmail.com>
2020-09-09 21:58:42 +0900

    TLA test1

--- home/system/php_libs/b2_shippinglist.php
+++ home/system/php_libs/b2_shippinglist.php
@@ -27,7 +27,7 @@ try{
         $sql2 .= ' and carriage = "'.$_REQUEST['carriage'].'"';
     }
     */
-    if($_REQUEST['readytoship']!=""){
+    if ($_REQUEST['readytoship']!="") {
         $sql2 .= ' and readytoship = "'.$_REQUEST['readytoship'].'"';
     }
     if(!empty($_REQUEST['shipped'])){
